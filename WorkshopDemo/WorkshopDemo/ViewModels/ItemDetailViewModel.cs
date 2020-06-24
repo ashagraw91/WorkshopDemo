ec82540ae4d571869562e48f933cbc992166eee7
Ashish Agrawal <Ashish.Agrawal@bcstechnology.com.au>
2020-06-24 17:22:38 +0530

    Add project files.

---
+++ WorkshopDemo/WorkshopDemo/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using WorkshopDemo.Models;
+
+namespace WorkshopDemo.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
